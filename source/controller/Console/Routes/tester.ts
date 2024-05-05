5dee1e475d022eee791d33f047ffda777c3d8376
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-05 16:25:21 +0100

    Tester 🧪

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,4 +1,3 @@
-import Market from "@/Core/Binance/Market"
 
 /*
 |-----------------------------
@@ -8,22 +7,6 @@ import Market from "@/Core/Binance/Market"
 |
 */
 export default async function () {
-
-    const market = new Market
-
-    const results: [string, number][] = []
-
-    for (const asset of await market.assets()) {
-
-        const [candle] = await asset.candles({ interval: "15m", limit: 1 })
-
-        results.push([asset.symbol, candle.changePercent])
-
-        console.log(asset.symbol)
-
-    }
-
-    console.table(results.sort((item1, item2) => item2[1] - item1[1]))
 
     console.log("The test completed successfully 🧪 ")
 }
