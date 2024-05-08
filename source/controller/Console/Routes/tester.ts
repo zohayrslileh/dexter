573b390c9db59f817a801729d814f0ff3683c913
Zohayr Slileh <zohayrslileh@gmail.com>
2024-05-08 23:02:38 +0100

    save

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,3 +1,4 @@
+import Account from "@/Core/Meta/Account"
 
 /*
 |-----------------------------
@@ -7,6 +8,14 @@
 |
 */
 export default async function () {
+
+    const account = new Account("a1109461-09ce-4364-bf01-31a280539fb7")
+
+    await account.connect()
+
+    const pairs = await account.pairs()
+
+    console.log(pairs.length)
 
     console.log("The test completed successfully 🧪 ")
 }
